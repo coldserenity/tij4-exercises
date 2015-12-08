d004f6526a43027b054468919df0e411b7d9f9a2
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-08 18:23:21 +0200

    Done Exercise 03

---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex03/ATypeName.java
@@ -0,0 +1,7 @@
+package edu.coldserenity.tij.ch01.ex03;
+
+public class ATypeName {
+    public static void main(String[] args) {
+        ATypeName aTypeName = new ATypeName();
+    }
+}
---
+++ 01-everything-is-an-object/src/test/java/edu/coldserenity/tij/ch01/ex03/ATypeNameTest.groovy
@@ -0,0 +1,13 @@
+package edu.coldserenity.tij.ch01.ex03
+
+import org.junit.Assert
+import org.junit.Test
+
+class ATypeNameTest {
+
+    @Test
+    void testMain() {
+        ATypeName.main();
+        Assert.assertTrue("Method worked well", true);
+    }
+}
