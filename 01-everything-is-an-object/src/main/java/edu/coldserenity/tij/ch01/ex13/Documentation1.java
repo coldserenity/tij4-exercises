f0c301575e3a1a9040099c54bb3255399c395c61
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-09 02:45:00 +0200

    Done Exercise 13

---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex13/Documentation1.java
@@ -0,0 +1,18 @@
+//: object/Documentation1.java
+package edu.coldserenity.tij.ch01.ex13;
+
+/**
+ * A class comment.
+ */
+public class Documentation1 {
+    /**
+     * A field comment.
+     */
+    public int i;
+
+    /**
+     * A method comment.
+     */
+    public void f() {
+    }
+} ///:~
---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex13/Documentation2.java
@@ -0,0 +1,11 @@
+//: object/Documentation2.java
+package edu.coldserenity.tij.ch01.ex13;
+
+/**
+ * <pre>
+ * System.out.println(new Date());
+ * </pre>
+ */
+public class Documentation2 {
+}
+///:~
