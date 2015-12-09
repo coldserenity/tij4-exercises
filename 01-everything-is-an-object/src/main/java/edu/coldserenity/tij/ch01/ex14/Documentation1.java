dcff1675464f76a6cb25dbc93ccac4b52394ccd9
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-09 02:49:52 +0200

    Done Exercise 14

---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex14/Documentation1.java
@@ -0,0 +1,32 @@
+//: object/Documentation1.java
+package edu.coldserenity.tij.ch01.ex14;
+
+/**
+ * A class comment.
+ *
+ * We've decided that this doc requires some HTML lists.
+ * It's as simple as:
+ * <ul>
+ *     <li>One</li>
+ *     <li>Two</li>
+ *     <li>Three</li>
+ * </ul>
+ *
+ * <ol>
+ *     <li>Twenty One</li>
+ *     <li>Twenty Two</li>
+ *     <li>Twenty Three</li>
+ * </ol>
+ */
+public class Documentation1 {
+    /**
+     * A field comment.
+     */
+    public int i;
+
+    /**
+     * A method comment.
+     */
+    public void f() {
+    }
+} ///:~
---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex14/Documentation3.java
@@ -0,0 +1,14 @@
+//: object/Documentation3.java
+package edu.coldserenity.tij.ch01.ex14;
+
+/**
+ * You can <em>even</em> insert a list:
+ * <ol>
+ * <li> Item one
+ * <li> Item two
+ * <li> Item three
+ * </ol>
+ */
+public class Documentation3 {
+}
+///:~
