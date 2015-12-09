c6d7eb29e0135f04ca8931d7100a99759d03ea30
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-09 02:56:01 +0200

    Done Exercise 15

---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex15/HelloWorld.java
@@ -0,0 +1,23 @@
+package edu.coldserenity.tij.ch01.ex15;
+
+/**
+ * An absolutely new HelloWorld.
+ * <p/>
+ * <b>Did you know that adding a dot in the first sentence is required when
+ * you have package comments? Otherwise package summary table will not be
+ * nicely generated.</b>
+ */
+public class HelloWorld {
+    /**
+     * The MAIN class to rule them all.
+     * <p/>
+     * Same applies to methods as far as I remember.
+     * So just turn on "First sentence must end with a dot" in your
+     * checkstyle!
+     *
+     * @param args an array of string arguments coming out of the void!
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
