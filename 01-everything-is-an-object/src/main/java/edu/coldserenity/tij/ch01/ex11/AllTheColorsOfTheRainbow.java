26b10e9a93ef7c04a7cc3f1030470a34d1fa3cbc
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-09 02:29:25 +0200

    Done Exercise 11

---
+++ 01-everything-is-an-object/src/main/java/edu/coldserenity/tij/ch01/ex11/AllTheColorsOfTheRainbow.java
@@ -0,0 +1,11 @@
+package edu.coldserenity.tij.ch01.ex11;
+
+public class AllTheColorsOfTheRainbow {
+    int anIntegerRepresentingColors;
+
+    void changeTheHueOfTheColor(int newHue) {
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ 01-everything-is-an-object/src/test/java/edu/coldserenity/tij/ch01/ex11/AllTheColorsOfTheRainbowTest.java
@@ -0,0 +1,16 @@
+package edu.coldserenity.tij.ch01.ex11;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AllTheColorsOfTheRainbowTest {
+
+    @Test
+    public void testMain() throws Exception {
+        AllTheColorsOfTheRainbow.main(new String [] {});
+        Assert.assertTrue("Method worked well", true);
+
+    }
+}
