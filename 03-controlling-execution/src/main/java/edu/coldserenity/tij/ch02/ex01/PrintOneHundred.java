33bf6104632672707f4206e751b8b469e4a47dfd
Roman Muntyanu <muntyanu.roman@gmail.com>
2015-12-10 13:03:27 +0200

    Done Chapter 03 / Exercise 01

---
+++ 03-controlling-execution/src/main/java/edu/coldserenity/tij/ch02/ex01/PrintOneHundred.java
@@ -0,0 +1,14 @@
+package edu.coldserenity.tij.ch02.ex01;
+
+/**
+ * Exercise 1: (1)
+ * <p/>
+ * Write a program that prints values from 1 to 100.
+ */
+public class PrintOneHundred {
+    public static void main(String[] args) {
+        for (int i=1; i<101; i++){
+            System.out.println(i);
+        }
+    }
+}
