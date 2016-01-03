60ea4fb8346980f87236dc030e7ac6435cde32ae
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-03 19:35:18 +0200

    Done Chapter 04 / Exercise 03

---
+++ 04-initialization-and-cleanup/src/main/java/edu/coldserenity/tij/ch04/ex03/DefaultConstructor.java
@@ -0,0 +1,18 @@
+package edu.coldserenity.tij.ch04.ex03;
+
+/**
+ * Exercise 3: (1)
+ * <p/>
+ * Create a class with a default constructor
+ * (one that takes no arguments) that prints a message.
+ * Create an object of this class.
+ */
+public class DefaultConstructor {
+    public DefaultConstructor() {
+        System.out.println("Message");
+    }
+
+    public static void main(String[] args) {
+        new DefaultConstructor();
+    }
+}
