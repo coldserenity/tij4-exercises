7a288dcdf511835006e7cd06183522ea4a99751d
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-09 18:51:30 +0200

    Done Chapter 06 / Exercise 02

---
+++ 06-reusing-classes/src/main/java/edu/coldserenity/tij/ch05/ex02/Antiseptic.java
@@ -0,0 +1,70 @@
+package edu.coldserenity.tij.ch05.ex02;
+
+/**
+ * Exercise 2: (2)
+ *
+ * Inherit a new class from class Detergent.
+ * Override scrub( ) and add a new method called sterilize( ).
+ */
+
+public class Antiseptic extends Detergent {
+
+    @Override
+    public void scrub() {
+        append(" Antiseptic.scrub()");
+        super.scrub();
+
+    }
+
+    public void sterilize() {
+        append(" sterilize()");
+    }
+
+    public static void main(String[] args) {
+        Antiseptic x = new Antiseptic();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        x.sterilize();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Detergent.main(args);
+    }
+}
+
+class Cleanser {
+    private String s = "Cleanser";
+    public void append(String a) { s += a; }
+    public void dilute() { append(" dilute()"); }
+    public void apply() { append(" apply()"); }
+    public void scrub() { append(" scrub()"); }
+    public String toString() { return s; }
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute(); x.apply(); x.scrub();
+        System.out.println(x);
+    }
+}
+
+class Detergent extends Cleanser {
+    // Change a method:
+    public void scrub() {
+        append(" Detergent.scrub()");
+        super.scrub(); // Call base-class version
+    }
+    // Add methods to the interface:
+    public void foam() { append(" foam()"); }
+    // Test the new class:
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Cleanser.main(args);
+    }
+}
+
