d26ad979832755bc0c48b5c86894ab6a3f560d86
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-14 18:02:04 +0200

    Done Chapter 09 / Exercise 22

---
+++ 08-interfaces/src/main/java/edu/coldserenity/tij/ch08/ex22/Sequence.java
@@ -0,0 +1,92 @@
+package edu.coldserenity.tij.ch08.ex22;
+
+/**
+ * Exercise 22: (2)
+ * <p/>
+ * Implement reverseSelector( ) in Sequence.java.
+ */
+public class Sequence {
+    private Object[] items;
+    private int next = 0;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length)
+            items[next++] = x;
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+
+        public boolean end() {
+            return i == items.length;
+        }
+
+        public Object current() {
+            return items[i];
+        }
+
+        public void next() {
+            if (i < items.length) i++;
+        }
+    }
+
+    private class ReverseSelector implements Selector {
+        private int i = items.length - 1;
+
+        @Override
+        public boolean end() {
+            return i == -1;
+        }
+
+        @Override
+        public Object current() {
+            return items[i];
+        }
+
+        @Override
+        public void next() {
+            if (i >= 0) i--;
+        }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+    public Selector reverseSelector() {
+        return new ReverseSelector();
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i = 0; i < 10; i++)
+            sequence.add(Integer.toString(i));
+        Selector selector = sequence.selector();
+        while (!selector.end()) {
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+
+        System.out.println("\nReverse selector: ");
+        Selector reverseSelector = sequence.reverseSelector();
+        while (!reverseSelector.end()) {
+            System.out.print(reverseSelector.current() + " ");
+            reverseSelector.next();
+        }
+
+
+    }
+
+}
+
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
