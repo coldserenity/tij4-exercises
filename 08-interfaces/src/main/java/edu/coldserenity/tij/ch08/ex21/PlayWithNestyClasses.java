58f19931c1f1906d211c6875cf88f917b779ab2d
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-14 17:07:15 +0200

    Done Chapter 09 / Exercise 21

---
+++ 08-interfaces/src/main/java/edu/coldserenity/tij/ch08/ex21/PlayWithNestyClasses.java
@@ -0,0 +1,40 @@
+package edu.coldserenity.tij.ch08.ex21;
+
+/**
+ * Exercise 21: (2)
+ * <p/>
+ * Create an interface that contains a nested class that has a static method
+ * that calls the methods of your interface and displays the results.
+ * Implement your interface and pass an instance of your implementation to the method.
+ */
+public class PlayWithNestyClasses {
+    public static void main(String[] args) {
+        Interface.NestyClass.display(new Impl());
+    }
+}
+
+interface Interface {
+    int f();
+
+    String g();
+
+    class NestyClass {
+        public static void display(Interface inf) {
+            System.out.println("inf.f(): " + inf.f());
+            System.out.println("inf.g(): " + inf.g());
+        }
+    }
+}
+
+class Impl implements Interface {
+
+    @Override
+    public int f() {
+        return 100;
+    }
+
+    @Override
+    public String g() {
+        return "100.500";
+    }
+}
