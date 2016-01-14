fdec591a9ecba4ddd4a2eac2ae1fc08cf0f81290
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-14 16:11:25 +0200

    Done Chapter 09 / Exercise 12

---
+++ 09-inner-classes/src/main/java/edu/coldserenity/tij/ch09/ex12/PrivateFieldsForInnerClasses.java
@@ -0,0 +1,47 @@
+package edu.coldserenity.tij.ch09.ex12;
+
+/**
+ * Exercise 12: (1)
+ * <p/>
+ * Repeat Exercise 7 using an anonymous inner class.
+ * <p/>
+ * Exercise 7: (2)
+ * <p/>
+ * Create a class with a private field and a private method. Create an
+ * inner class with a method that modifies the outer-class field and
+ * calls the outer-class method.
+ * In a second outer-class method, create an object of the inner class
+ * and call its method, then show the effect on the outer-class object.
+ */
+public class PrivateFieldsForInnerClasses {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        System.out.println("Initial state of new Outer object: \n" + outer);
+        outer.secondMethod();
+        System.out.println("\nAfter calling Outer.secondMethod(): \n" + outer);
+    }
+}
+
+class Outer {
+    private Integer privateField = 100;
+
+    private void privateMethod() {
+        System.out.println("Outer.privateMethod() was triggered");
+    }
+
+    public void secondMethod() {
+        new Object() {
+            void trickyMethod() {
+                privateField = 300;
+                privateMethod();
+            }
+        }.trickyMethod();
+    }
+
+    @Override
+    public String toString() {
+        return "Outer{" +
+                "privateField=" + privateField +
+                '}';
+    }
+}
