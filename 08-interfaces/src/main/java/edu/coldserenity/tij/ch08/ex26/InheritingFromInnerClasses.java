ff66e0c5698d2aabd1372c9ab136ded6c72d5111
Roman Muntyanu <muntyanu.roman@gmail.com>
2016-01-15 12:38:46 +0200

    Done Chapter 09 / Exercise 26

---
+++ 08-interfaces/src/main/java/edu/coldserenity/tij/ch08/ex26/InheritingFromInnerClasses.java
@@ -0,0 +1,48 @@
+package edu.coldserenity.tij.ch08.ex26;
+
+/**
+ * Exercise 26: (2)
+ * <p/>
+ * Create a class with an inner class that has a non-default constructor
+ * (one that takes arguments). Create a second class with an inner class
+ * that inherits from the first inner class.
+ */
+public class InheritingFromInnerClasses {
+    public static void main(String[] args) {
+        System.out.println(new InheritInner(new WithInner(10)));
+    }
+}
+
+class WithInner {
+    int field;
+
+    public WithInner(int field) {
+        this.field = field;
+    }
+
+    class Inner {
+    }
+
+    @Override
+    public String toString() {
+        return "WithInner{" +
+                "field=" + field +
+                '}';
+    }
+}
+
+class InheritInner extends WithInner.Inner {
+    private WithInner wi;
+
+    public InheritInner(WithInner wi) {
+        wi.super();
+        this.wi = wi;
+    }
+
+    @Override
+    public String toString() {
+        return "InheritInner{" +
+                "\n  " + wi +
+                "\n}";
+    }
+}
